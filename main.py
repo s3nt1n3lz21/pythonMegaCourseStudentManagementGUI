47016218f7682eb51def224e03731a6db7890bd1
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-10-18 14:58:41 +0100

    Created a table in the GUI and loaded data into it

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QLabel,
-                             QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
-                             )
+                             QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
+                            )
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -20,7 +21,25 @@ class MainWindow(QMainWindow):
         # Fix to show help menu on MAC OS
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        # Hide index column
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
