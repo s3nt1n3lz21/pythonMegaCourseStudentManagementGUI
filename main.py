04899703bb7d962e950713350cd1fe69c482d01a
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-10-18 17:48:56 +0100

    Added shortcut moveable toolbar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QLabel,
                              QWidget, QGridLayout, QLineEdit,
-                             QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox
+                             QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox, QToolBar
                              )
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QIcon
 import sys
 import sqlite3
 
@@ -11,12 +11,13 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 800)
 
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
         edit_menu_item = self.menuBar().addMenu("&Edit")
 
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
@@ -25,7 +26,7 @@ class MainWindow(QMainWindow):
         # Fix to show help menu on MAC OS
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
-        search_action = QAction("Search", self)
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
         search_action.triggered.connect(self.search)
         edit_menu_item.addAction(search_action)
 
@@ -35,6 +36,13 @@ class MainWindow(QMainWindow):
         # Hide index column
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+        # Create shortcut toolbar
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
